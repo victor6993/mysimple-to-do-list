a6b2fa308a9341db7e7c9d7a2be544ac2a5c2ca8
victor wang <victor.wang@bootcamp-upgrade.com>
2020-04-21 11:10:57 +0200

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -129,6 +130,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started on port"+ port);
 });
